ada053092e19e2733a843d1945ea19e599691f21
codemathsz <mo284891@gmail.com>
2025-02-19 18:42:04 -0300

    feat: create project provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import "./App.css";
 import "react-day-picker/style.css";
 import { AuthProvider } from "@/context/AuthContext";
 import { Router } from "./Router";
+import { ProjectProvider } from "./context/ProjectContext";
 
 export function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <ProjectProvider>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ProjectProvider>
     </AuthProvider>
   );
 }
