50c9f05628650acb644ce9cb9a0fa5ff8cbd9f1a
codemathsz <mo284891@gmail.com>
2025-02-19 14:01:09 -0300

    feat: remove theme borderRadius

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,11 +16,6 @@ const config: Config = {
 			fontFamily:{
 				poppins: ['Poppins', 'sans-serif'],
 			},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
   		keyframes: {
   			'accordion-down': {
   				from: {
